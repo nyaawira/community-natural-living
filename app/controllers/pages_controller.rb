8fbf3e5d889af43ab4fb45417e6abc4ffee0cf13
Sam Nyavor <samnyavor@gmail.com>
2016-08-31 12:24:00 +0000

    certifying

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def letsencrypt
+    # use your code here, not mine
+    render text: "lKlW_fbCNIXzgSuqaPpIJrrK4tntS5VaYMtJZ9SelQs.jIJ2NVv9b300Z56Z-9rmBYbNZle4i6FdfiQANzIuHKg"
+  end
+end
